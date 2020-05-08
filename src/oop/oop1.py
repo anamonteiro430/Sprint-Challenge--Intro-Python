46c4c4c9380d64491520441e12f18d9a0765c199
kika <anamonteiro430@gmail.com>
2020-05-08 15:37:12 +0100

    oop2.py

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -1,59 +1,12 @@
-# Write classes for the following class hierarchy:
-#
-#  [Vehicle]->[FlightVehicle]->[Starship]
-#      |                |
-#      v                v
-# [GroundVehicle]      [Airplane]
-#   |       |
-#   v       v
-# [Car]  [Motorcycle]
-#
-# Each class can simply "pass" for its body. The exercise is about setting up
-# the hierarchy.
-#
-# e.g.
-#
-# class Whatever:
-#     pass
-#
-# Put a comment noting which class is the base class
-
-# Write classes for the following class hierarchy:
-#
-#  [Vehicle]->[FlightVehicle]->[Starship]
-#      |                |
-#      v                v
-# [GroundVehicle]      [Airplane]
-#   |       |
-#   v       v
-# [Car]  [Motorcycle]
-#
-# Each class can simply "pass" for its body. The exercise is about setting up
-# the hierarchy.
-#
-# e.g.
-#
-# class Whatever:
-#     pass
-#
-# Put a comment noting which class is the base class
-
-
 class Vehicle(): #base class
-     def __init__(self, color, velocity):
-          self.color = color
-          self.velocity = velocity
+     pass
      
 
 class GroundVehicle(Vehicle): ##base class for Car and Motorcycle
-     def __init__(self, color, velocity, wheels):
-         super().__init__(color, velocity)
-         self.wheels = wheels
+     pass
 
 class FlightVehicle(Vehicle): ##base class for Airplane and Starship
-     def __init__(self, color, velocity, seats):
-          super().__init__(color, velocity)
-          self.seats = seats
+     pass
 
 class Car(GroundVehicle):
      pass
--- src/oop/test_oop2.py
+++ src/oop/test_oop2.py
@@ -1,5 +1,47 @@
 import unittest
-from oop2 import *
+# To the GroundVehicle class, add method drive() that returns "vroooom".
+#
+# Also change it so the num_wheels defaults to 4 if not specified when the
+# object is constructed.
+
+class GroundVehicle():
+    def __init__(self, num_wheels = 4):
+        self.num_wheels = num_wheels #default
+
+    def __str__(self):
+        return f"{self.num_wheels} wheels."
+    #drive() method
+    def drive(self):
+        return f"vroooom"
+
+
+# Subclass Motorcycle from GroundVehicle.
+class Motorcycle(GroundVehicle):
+    def __init__(self, num_wheels = 2):
+        super().__init__(num_wheels)
+        self.num_wheels = num_wheels
+# Make it so when you instantiate a Motorcycle, it automatically sets the number
+# of wheels to 2 by passing that to the constructor of its superclass.
+    def __str__(self):
+        return f"{self.num_wheels} wheels."
+# Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
+    def drive(self):
+        return f"BRAAAP!!"
+
+    
+
+vehicles = [
+    GroundVehicle(),
+    GroundVehicle(),
+    Motorcycle(),
+    GroundVehicle(),
+    Motorcycle(),
+]
+
+# Go through the vehicles list and print the result of calling drive() on each.
+for i in vehicles:
+    print(i.drive())
+
 
 class Oop2Tests(unittest.TestCase):
   def setUp(self):
